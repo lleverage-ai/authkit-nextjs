75d310edae19cbddf0e231864489263da0d80864
Alex Jay <alex@fridaycollective.co.uk>
2024-12-06 15:20:32 +0100

    update middleware to pass back response

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { NextMiddleware } from 'next/server';
+import { NextMiddleware, NextResponse } from 'next/server';
 import { updateSession } from './session.js';
 import { AuthkitMiddlewareOptions } from './interfaces.js';
 import { WORKOS_REDIRECT_URI } from './env-variables.js';
@@ -9,7 +9,22 @@ export function authkitMiddleware({
   redirectUri = WORKOS_REDIRECT_URI,
   signUpPaths = [],
 }: AuthkitMiddlewareOptions = {}): NextMiddleware {
-  return function (request) {
-    return updateSession(request, debug, middlewareAuth, redirectUri, signUpPaths);
+  return async function (request, event) {
+    console.log({ event });
+
+    const sessionResponse = await updateSession(request, debug, middlewareAuth, redirectUri, signUpPaths);
+
+    // Always return a NextResponse that can be modified
+    if (sessionResponse instanceof Response) {
+      // Convert the Response to a NextResponse if it isn't already
+      return sessionResponse instanceof NextResponse
+        ? sessionResponse
+        : NextResponse.next({
+            status: sessionResponse.status,
+            headers: sessionResponse.headers,
+          });
+    }
+
+    return NextResponse.next();
   };
 }
